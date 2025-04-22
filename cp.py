724e5f9cfe747b35ddd8008d67f3d2b4823086a7
mxmzhang <144863474+mxmzhang@users.noreply.github.com>
2025-04-22 17:36:35 -0400

    cp edits

--- cp.py
+++ cp.py
@@ -2,6 +2,7 @@ from ortools.sat.python import cp_model
 from typing import Optional, Tuple
 from typing import List
 import numpy as np
+import json
 
 # assume that we have a list of recipes from spoonacular called recipes, a 2D matrix
 def process_recipes(recipes):
@@ -50,10 +51,10 @@ def process_recipes(recipes):
 def cp(data, budget, calorie_cap, chosen_meals = 5):
     model = cp_model.CpModel()
     rlen = len(data["recipes"])
-    ilen = len(data["ingredients"])
+    ilen = len(data["all_ingredients"])
 
-    recipes = data.get("recipes", {})
-    ingredients = data.get("ingredients", {})
+    recipes = data.get("recipes", [])
+    ingredients = data.get("all_ingredients", [])
 
     # x_r: 0/1 if we choose recipe r
     x = {}
@@ -72,17 +73,21 @@ def cp(data, budget, calorie_cap, chosen_meals = 5):
     model.Add(sum(x[rid] for rid in range(rlen)) == chosen_meals)
 
     # sum of usage for each ingredient i across chosen recipes <= inventory + b[i]
-    inventory = data.get("inventory", {})
+
+    # inventory = data.get("inventory", {})
     for i in range(ilen):
-        inv_i = inventory.get(i, 0)
+        # inv_i = inventory.get(i, 0)
+        # print("ing id:", i)
         usage_expr = []
         for rid in range(rlen):
             needed = 0
-
-            for ing_r in recipes.get("ingredients", {}):
+            for ing_r in recipes[rid]["ingredients"]:
+                # print(ing_r)
                 if ing_r.get("i_id") == i:
                     needed = ing_r.get("proportion")
-            usage_expr.append(needed * x[rid])
+            #         print('needed')
+            # print(int(needed * 100) )
+            usage_expr.append(int(needed * 100) * x[rid])
 
 
             # if rid in data["recipe_ingredients"]:
@@ -92,29 +97,30 @@ def cp(data, budget, calorie_cap, chosen_meals = 5):
             #             break
             # usage_expr.append(needed * x[rid])
 
-        model.Add(sum(usage_expr) <= inv_i + b[i])
+        # model.Add(sum(usage_expr) <= inv_i + b[i])
+        model.Add(sum(usage_expr) <= b[i])
 
     # sum of cost of purchased ingredients <= budget
     cost_expr = []
     for i in range(ilen):
-        cost_per_unit = ingredients[i]
+        cost_per_unit = ingredients[i]["unit_price"]
         cost_expr.append(cost_per_unit * b[i])
-    model.Add(sum(cost_expr) <= budget)
+    model.Add(sum(cost_expr) <= budget*100*100)
 
     for rid in range(rlen):
-        model.Add(recipes[rid].get("nutrition",{})["calories"] * x[rid] <= calorie_cap)
+        model.Add(round(recipes[rid].get("nutrients",{})["calories"]) * x[rid] <= calorie_cap)
 
     # objective function
     total_protein = model.NewIntVar(0, 10000000, "total_protein")
-    model.Add(total_protein == sum(recipes[rid].get("nutrition", {})["protein"] * x[rid] for rid in len(rlen)))
+    model.Add(total_protein == sum(round(recipes[rid].get("nutrients", {})["protein"]) * x[rid] for rid in range(rlen)))
 
     total_calories = model.NewIntVar(0, 10000000, "total_calories")
-    model.Add(total_calories == sum(recipes[rid].get("nutrition", {})["calories"] * x[rid] for rid in len(rlen)))
+    model.Add(total_calories == sum(round(recipes[rid].get("nutrients", {})["calories"]) * x[rid] for rid in range(rlen)))
 
     obj_expr = model.NewIntVar(-100000000, 100000000, "obj_expr")
 
     # make sure units of things in objective function are scaled the same
-    model.Add(obj_expr == (3*total_protein - total_calories)) # test/experiment with objective functions
+    model.Add(obj_expr == (6*total_protein - total_calories)) # test/experiment with objective functions
     model.Maximize(obj_expr)
 
     solver = cp_model.CpSolver()
@@ -139,11 +145,34 @@ def cp(data, budget, calorie_cap, chosen_meals = 5):
         for i in range(ilen):
             amt = purchased[i]
             if amt > 0:
-                print(f"   Ingredient {i} ({data['ingredients'][i]['name']}): {amt}")
+                print(f"Ingredient {i} ({ingredients[i]['name']}): {amt/100.0}")
 
-        print(f"Total cost used: {sum(ingredients[i].get("price")*purchased[i] for i in range(ilen))} (Budget = {budget})")
+        # for i in range(ilen):
+        #     print(solver.Value(b[i]))
+        print(f"Total cost used: {sum(ingredients[i].get("unit_price")*purchased[i]/(100.0*100.0) for i in range(ilen))} (Budget = {budget})")
         print("Total Protein:", solver.Value(total_protein))
         print("Total Calories:", solver.Value(total_calories))
 
     else:
         print("No feasible solution found.")
+
+def main():
+    file = "preprocessing/combined_recipe_data.json"
+    try:
+        with open(file, 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        print(f"Error: File not found at '{file}'")
+        return
+    except json.JSONDecodeError:
+        print(f"Error: Invalid JSON format in '{file}'")
+        return
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return
+    
+    cp(data, 200, 460)
+
+if __name__ == '__main__':
+    main()
+
